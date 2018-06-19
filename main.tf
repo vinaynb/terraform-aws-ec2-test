9d3b3bb15ac9d21359bdba61463a50ba30fbe783
Vinay Bhinde <vinaynb@gmail.com>
2018-06-19 10:34:09 +0530

    made module more simple with just 3 input params

--- main.tf
+++ main.tf
@@ -1,24 +1,5 @@
-locals {
-  subnet_ids           = ["${split(",", var.network_configuration["subnet_ids"])}"]
-  security_group_ids   = ["${split(",", var.security_configuration["security_group_ids"])}"]
-  iam_instance_profile = "${lookup(var.security_configuration, "iam_instance_profile", "")}"
-  enable_public        = "${lookup(var.network_configuration, "enable_public", "false")}"
-}
-
 resource "aws_instance" "simple" {
-	count         = "${var.count}"
-	ami           = "${var.ami}"
-	instance_type = "${var.instance_type}"
-
-	associate_public_ip_address = "${local.enable_public}"
-	subnet_id                   = "${element(local.subnet_ids, count.index % length(local.subnet_ids))}"
-	vpc_security_group_ids      = ["${local.security_group_ids}"]
-	iam_instance_profile        = "${local.iam_instance_profile}"
-
-	tags = "${merge(
-			var.tags,
-			map(
-				"Name", format("%s-%d", var.name_prefix, count.index + 1)
-			)
-		)}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id     = "${var.subnet_id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,34 +1,12 @@
-variable "name_prefix" {
-	description = "Name prefix for EC2 instance(s)"
-	default		= "test"
-}
-
-variable "count" {
-	default     = "1"
-	description = "Number of EC2 instance(s) to be provisioned"
-}
-
 variable "instance_type" {
-	default     = "t2.micro"
-	description = "Type of EC2 instance"
+  default     = "t2.nano"
+  description = "Type of EC2 instance"
 }
 
 variable "ami" {
-	description = "AMI used to provision"
+  description = "AMI used to provision"
 }
 
-variable "network_configuration" {
-	type        = "map"
-	description = "Network configuration (enable_public, subnet_ids) used when provisioning EC2 instance(s)"
-}
-
-variable "security_configuration" {
-	type        = "map"
-	description = "Security configuration (security_group_ids, iam_instance_profile) used when provisioning EC2 instance(s)"
-}
-
-variable "tags" {
-	type        = "map"
-	description = "Addtional tags for EC2 instance(s)"
-	default     = {}
+variable "subnet_id" {
+  description = "Subnet to provision this ec2"
 }
